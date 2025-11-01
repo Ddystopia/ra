51e7ea1149585e919fcdb94f2f0bde134d5e178e
Oleksandr Babak <alexanderbabak@proton.me>
2025-11-01 16:01:13 +0100

    chore: update old drivers for new Block trait

--- ra-fsp-rs/src/smoltcp/ether.rs
+++ ra-fsp-rs/src/smoltcp/ether.rs
@@ -1,5 +1,5 @@
 use {
-    crate::ether::{self, Buffer, EtherInstance},
+    crate::{ether::{self, Buffer, Ether}, state_markers::Opened},
     core::{cell::RefCell, pin::Pin},
     ra_fsp_sys::r_ether::InterruptCause,
     smoltcp::{
@@ -9,24 +9,24 @@ use {
 };
 
 pub struct Dev<const MTU: usize> {
-    eth: RefCell<Pin<&'static mut EtherInstance<MTU>>>,
+    eth: RefCell<Pin<&'static mut Ether<MTU, Opened>>>,
     capabilities: DeviceCapabilities,
 }
 
 pub struct EthernetRxToken<'a, const MTU: usize>(
     Option<Pin<&'static mut Buffer<MTU>>>,
     usize,
-    &'a RefCell<Pin<&'static mut EtherInstance<MTU>>>,
+    &'a RefCell<Pin<&'static mut Ether<MTU, Opened>>>,
 );
 
 pub struct EthernetTxToken<'a, const MTU: usize>(
     Option<Pin<&'static mut Buffer<MTU>>>,
-    &'a RefCell<Pin<&'static mut EtherInstance<MTU>>>,
+    &'a RefCell<Pin<&'static mut Ether<MTU, Opened>>>,
 );
 
 impl<const MTU: usize> Dev<MTU> {
     pub fn new(
-        eth: Pin<&'static mut EtherInstance<MTU>>,
+        eth: Pin<&'static mut Ether<MTU, Opened>>,
         capabilities: DeviceCapabilities,
     ) -> Self {
         assert!(
@@ -40,7 +40,7 @@ impl<const MTU: usize> Dev<MTU> {
         }
     }
 
-    pub fn eth(&mut self) -> Pin<&mut EtherInstance<MTU>> {
+    pub fn eth(&mut self) -> Pin<&mut Ether<MTU, Opened>> {
         self.eth.get_mut().as_mut()
     }
 
