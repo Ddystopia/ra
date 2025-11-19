f1edcb3017dab239accd4ddab39aea5dd5b6b170
Oleksandr Babak <alexanderbabak@proton.me>
2025-11-19 10:53:50 +0100

    feat(ra-fsp-rs): add utility to set interrupt's priority as fsp logical priority

--- ra-fsp-rs/src/utils.rs
+++ ra-fsp-rs/src/utils.rs
@@ -10,10 +10,14 @@ pub(crate) const fn extract_irq(this: Option<pac::Interrupt>) -> i32 {
 }
 
 // fixme: not true on __CORTEX_M == 23
+/// Convert FSP priority to hardware priority: Smaller value means higher priority.
+/// Note that RTIC uses reversed priority scheme - smaller value means lower priority.
 pub const fn fsp_prio_to_hw(priority: u8, nvic_prio_bits: u8) -> u8 {
     (((priority as u32) << (8 - nvic_prio_bits) as u32) & (u8::MAX as u32)) as u8
 }
 
+/// Convert hardware priority to FSP priority: Smaller value means higher priority.
+/// Note that RTIC uses reversed priority scheme - smaller value means lower priority.
 pub const fn hw_prio_to_fsp(hw_priority: u8, nvic_prio_bits: u8) -> u8 {
     ((hw_priority as u32) >> (8 - nvic_prio_bits)) as u8
 }
@@ -25,6 +29,17 @@ pub fn read_fsp_priority<I: InterruptNumber>(irq: I) -> u8 {
     let hw_priority = NVIC::get_priority(irq);
 
     hw_prio_to_fsp(hw_priority, pac::NVIC_PRIO_BITS)
+}
+
+/// # Safety
+///
+/// Changing priority levels can break priority-based critical sections
+/// (see [`register::basepri`]) and compromise memory safety.
+///
+/// [`register::basepri`](https://docs.rs/cortex-m/0.7.7/cortex_m/register/basepri/index.html)
+pub unsafe fn set_fsp_priority<I: InterruptNumber>(nvic: &mut NVIC, irq: I, fsp_priority: u8) {
+    let hw_priority = fsp_prio_to_hw(fsp_priority, pac::NVIC_PRIO_BITS);
+    unsafe { nvic.set_priority(irq, hw_priority) }
 }
 
 pub(crate) fn try_read_priority_into(interrupt: impl Into<Option<pac::Interrupt>>, place: &mut u8) {
